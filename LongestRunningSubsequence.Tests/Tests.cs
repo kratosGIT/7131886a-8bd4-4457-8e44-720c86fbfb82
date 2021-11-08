6a3c3da46cc679e0d9cb74fb291cb0e50aafb2ac
Kratos <ganeshkawade2003@gmail.com>
2021-11-08 16:33:16 +1100

    created project struture and added a single test

---
+++ LongestRunningSubsequence.Tests/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using LongestRunningSubsequence;
+
+namespace LongestRunningSubsequence.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public async Task TestCase1()
+        {
+            var algorithm = new LongestRunningSubsequence.Algorithm("6 1 5 9 2");
+            var result = await algorithm.Run();
+            Assert.AreEqual("1 5 9", result);
+        }
+    }
+}
---
+++ LongestRunningSubsequence/Algorithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LongestRunningSubsequence
+{
+    public class Algorithm
+    {
+        private string Input;
+        public Algorithm(string input)
+        {
+            Input = input;
+        }
+
+        public async Task<string> Run()
+        {
+            var lrs = await Task.Run(()=>
+            {
+                return "";
+            });
+
+            return lrs;
+        }
+    }
+}
